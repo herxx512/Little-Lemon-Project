e04348d257c02e6c39e5040c07072e1d67907d56
Bruce Her <bruce.k.her@gmail.com>
2024-01-29 13:11:42 -0600

    Added Menu and Booking API

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    inventory = models.IntegerField(5)
+    inventory = models.IntegerField()
     
 class Booking(models.Model):
     name = models.CharField(max_length=255)
-    guests = models.IntegerField(6)
+    guests = models.IntegerField()
     date = models.DateField()
